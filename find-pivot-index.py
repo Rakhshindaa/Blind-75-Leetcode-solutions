ef7aa3e9e8a00c45d2d8a7221fd44331f570b2a0
Rakhshindaa <rakhshindanausheen@gmail.com>
2025-05-26 22:35:35 +0530

    Add files via upload

---
+++ find-pivot-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftsum=0
+        total=sum(nums)
+        for i in range(len(nums)):
+            if leftsum == total-leftsum-nums[i]:
+                return i
+            leftsum=leftsum+nums[i]
+        return -1
